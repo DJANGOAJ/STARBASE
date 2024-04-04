dfce7d0afe49527fbc576010bb4a22d3195e7eca
DJANGOAJ <154641128+DJANGOAJ@users.noreply.github.com>
2024-04-05 01:20:07 +0200

    Update SLY_Assistant.user.js

--- SLY_Assistant.user.js
+++ SLY_Assistant.user.js
@@ -6,9 +6,9 @@
 // @author       SLY w/ Contributions by niofox, SkyLove512, anthonyra
 // @match        https://*.based.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
-// @require      https://github.com/ImGroovin/SLY-Assistant/raw/main/anchor-browserified.js
-// @require      https://github.com/ImGroovin/SLY-Assistant/raw/main/buffer-browserified.js
-// @require      https://github.com/ImGroovin/SLY-Assistant/raw/main/bs58-browserified.js
+// @require      https://github.com/DJANGOAJ/STARBASE/raw/main/anchor-browserified.js
+// @require      https://github.com/DJANGOAJ/STARBASE/raw/main/buffer-browserified.js
+// @require      https://github.com/DJANGOAJ/STARBASE/raw/main/bs58-browserified.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=staratlas.com
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -19,17 +19,16 @@
 	'use strict';
 
 	//Used for reading solana data
-	let readRPCs = [
+	let readRPCs = ['https://solana-api.syndica.io/access-token/WPoEqWQ2auQQY1zHRNGJyRBkvfOLqw58FqYucdYtmy8q9Z84MBWwqtfVf8jKhcFh/rpc', //Syndica (Old)
 		'https://rpc.hellomoon.io/57dbc69d-7e66-4454-b33e-fa6a4b46170f', //Hello Moon
         'https://staratl-mainc06-2415.mainnet.rpcpool.com', //Triton
         'https://mainnet.helius-rpc.com/?api-key=735486d8-ae86-4d26-829c-e34a2210d119', //Helius
         'https://twilight-autumn-diagram.solana-mainnet.quiknode.pro/4fc53d638efd1cc0f80764bc457944bb325d1ff1', //Quicknode
-		'https://solana-api.syndica.io/access-token/WPoEqWQ2auQQY1zHRNGJyRBkvfOLqw58FqYucdYtmy8q9Z84MBWwqtfVf8jKhcFh/rpc', //Syndica (Old)
+		
 	];
 
 	//Used for pushing transactions to solana chain
-	let writeRPCs = [
-        'https://twilight-autumn-diagram.solana-mainnet.quiknode.pro/4fc53d638efd1cc0f80764bc457944bb325d1ff1', //Quicknode
+	let writeRPCs = ['https://blissful-long-bush.solana-mainnet.quiknode.pro/ef8c93c33d3c948bb8062f34540eb6b608b4ceae/','https://twilight-autumn-diagram.solana-mainnet.quiknode.pro/4fc53d638efd1cc0f80764bc457944bb325d1ff1', //Quicknode
 		'https://rpc.hellomoon.io/57dbc69d-7e66-4454-b33e-fa6a4b46170f', //Hello Moon
         'https://staratl-mainc06-2415.mainnet.rpcpool.com', //Triton
         'https://mainnet.helius-rpc.com/?api-key=735486d8-ae86-4d26-829c-e34a2210d119', //Helius
